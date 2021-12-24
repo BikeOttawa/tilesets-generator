767717ffac8a9afc0b516ec5389a160959acbfaa
zzptichka <shkvorets@gmail.com>
2021-12-24 15:16:36 -0500

    fix multiple files bug

--- src/jsonl.js
+++ src/jsonl.js
@@ -1,26 +1,28 @@
 import { createWriteStream, createReadStream } from "fs";
 import { parse } from "geojson-stream";
 
+async function convertJson(json, jsonlPath) {
+  const ldgeojson = createWriteStream(jsonlPath, {flags: "a"});
+  return new Promise((resolve, reject) => {
+    createReadStream(json.path)
+      .pipe(parse(row => {
+        if (row.geometry?.coordinates === null) return null;
+        row.properties["tag"] = json["tag"];
+        return (JSON.stringify(row) + "\r\n");
+      }))
+      .pipe(ldgeojson)
+      .on("finish", () => { resolve(true); })
+      .on("error", reject);
+  })
+}
+
 export async function convert(jsons, jsonlPath) {
+  createWriteStream(jsonlPath);
   try {
-    const ldgeojson = createWriteStream(jsonlPath);
-    const promises = jsons.map(json => new Promise((resolve, reject) => {
-      createReadStream(json.path)
-        .pipe(parse(row => {
-          if (row.geometry?.coordinates === null) {
-            return null;
-          }
-          row.properties["tag"] = json["tag"];
-          return (JSON.stringify(row) + "\r\n");
-        }))
-        .pipe(ldgeojson)
-        .on("finish", () => {
-          resolve(true);
-        })
-        .on("error", reject);
-    }))
-    await Promise.all(promises);
+    for(const json of jsons){
+      await convertJson(json, jsonlPath)
+    }
   } catch (err) {
-    console.log(err);
+    console.log("FAILED", err);
   }
 }
